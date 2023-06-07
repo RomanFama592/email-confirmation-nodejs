246aea036598f27a4660c868f8c41de0a36a0b41
Famá Román <famaroman@gmail.com>
2023-06-06 23:40:23 -0300

    developed logger

--- utils/logger.mjs
+++ utils/logger.mjs
@@ -1,6 +1,19 @@
-import moment from "moment-timezone";
+function addZero(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+function formaterTime(timeObject) {
+  //format: "HH:mm - DD/MM/YY"
+  return `${addZero(timeObject.getHours())}:${addZero(
+    timeObject.getMinutes()
+  )} - ${addZero(timeObject.getDate())}/${addZero(
+    timeObject.getMonth() + 1
+  )}/${timeObject.getFullYear().toString().slice(2)}`;
+}
 
 export default function logger(...output) {
-  const time = new Date().getTime();
-  console.log(...output, " :: ", new Date(time));
+  let msg = formaterTime(new Date()) + " => " + output.join();
+  //msg =  msg + "\n" + "-".repeat(msg.length)
+
+  console.log(msg);
 }
